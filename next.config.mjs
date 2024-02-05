ae18e2fd7a5e9547af41a15a94ce1c7b4a68202a
lex1olnk <lex1olnk@gmail.com>
2024-02-06 01:50:35 +0300

    deleted next export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-   
-    // Optional: Change the output directory `out` -> `dist`
-     distDir: 'dist',
 }
 
 export default nextConfig;
